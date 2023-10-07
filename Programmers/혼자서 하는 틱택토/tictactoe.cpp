c0c6d43c1e19d26391ccdcd3dca2ff2f3bca65f4
SanghoYoon <100823955+Sangh0Yoon@users.noreply.github.com>
2023-10-07 13:46:20 +0900

    Update tictactoe.cpp

--- Programmers/혼자서 하는 틱택토/tictactoe.cpp
+++ Programmers/혼자서 하는 틱택토/tictactoe.cpp
@@ -1,14 +1,28 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
+int det_win(const vector<string>& board, char ox){
+    vector<int> row(3), col(3), dia(2);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[i][j] == ox){
+                row[i]++;
+                col[j]++;
+                if(i == j) {dia[0]++;}
+                if(i+j == 2) {dia[1]++;}
+                if(row[i] == 3 || col[j] == 3 || dia[0] == 3 || dia[1] == 3) return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int solution(vector<string> board) {
     int answer = 1;
-    int num_o = 0;
-    int num_x = 0;
-    int num_empty = 0;
+    int num_o = 0, num_x = 0;
+    int win_o = 0, win_x = 0;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             if(board[i][j] == 'O'){
@@ -17,23 +31,20 @@ int solution(vector<string> board) {
             else if(board[i][j] == 'X'){
                 num_x++;
             }
-            else{
-                num_empty++;
-            }
         }
     }
     
-    // X가 O보다 더 많은 경우(O가 0개인 경우도 포함)(X가 5개 이상인것도 포함)
-    if(num_x > num_o){
+    if(num_x > num_o or num_o - num_x >= 2){
         answer = 0;
     }
-    // X와 O의 개수가 2개 이상 차이날 경우
-    else if(num_o - num_x >= 2){
-        answer = 0;
-    }
-    // X와 O가 1줄이 동시에 완성되는 경우(123,456,789,147,258,369,159,357)
-    else if(num_o == 3 and num_x == 3){
-        answer = 0;
+    
+    if(num_o >= 3){
+        win_o = det_win(board, 'O');
+        win_x = det_win(board, 'X');
+        
+        if((win_o and win_x) or (win_o and (num_o == num_x)) or (win_x and (num_o > num_x))){
+            answer = 0;
+        }
     }
         
     return answer;
